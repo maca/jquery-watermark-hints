12e0942d59cbd44fdf07c19875fd52a2b89eeb89
Macario Ortega <macario@macario.local>
2010-04-09 16:46:19 -0500

    repo created

---
+++ Readme.markdown
@@ -0,0 +1,13 @@
+# jquery-watermark-hints
+
+Simple jQuery plugin to provide watermark hints for text input fields and text areas.
+
+Example:
+  <input id="telephone" name="telephone" type="text" value="value for name"/>
+  
+  <script type="text/javascript" charset="utf-8">
+    $("name").hint('+52 (55) 56581591')
+  </script>
+  
+
+Hint text will appear in the field when is blank and will not be sent when submitting the form.
---
+++ jquery.watermark.hints.js
@@ -0,0 +1,33 @@
+// jQuery plugin by Macario Ortega
+// http://github.com/maca/jquery-watermark-hints
+
+jQuery.fn.hint = function(text, color) {
+  if(!color)
+    color = "#aaa";
+    
+  return this.each(
+    function(){
+      var input        = jQuery(this);
+      var defaultColor = input.css("color");
+
+      var checkInput = function() {
+        if(input.val() == '' || input.virgin){
+          input.val(text).css("color", color).virgin = true;
+        } else {
+          input.css("color", defaultColor).virgin = false;
+        }
+      };
+      
+      var clearMessage = function() {
+        if(input.virgin) 
+          input.val('').css("color", defaultColor).virgin = false;
+      };
+      
+      input.bind('blur change', checkInput);
+      input.bind('click focus', clearMessage);
+      checkInput();
+      
+      input.closest('form').bind('submit', clearMessage);
+    }
+  );
+};
